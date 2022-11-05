210844d2f3536b590a699dfc7122d72b8bb14c97
Emanuel Joness <itzemanuelj@gmail.com>
2022-11-05 00:06:01 -0400

    mvp commit for adding and removing elements in js

---
+++ Arrays: adding and removing elements.js
@@ -0,0 +1,97 @@
+// This code declares an empty array. Complete it.
+var finalScores = [];
+
+// counties is an empty array. What is the value of counties[0]?
+console.log(contries[0]);
+//undefined
+
+// What keyword removes the last element from an array?
+countries.pop();
+
+//The pop() method removes the last element from an array:
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.pop();
+//>> "Mango"
+
+// What keyword adds one or more elements to the end of an array?
+fruits.push('dragon fruit');
+
+//JS array .push() method
+
+let items_in_backpack = ['food', 'water', 'flashlight', 'GPS']; // Our starting array
+items_in_backpack.push('javascript array push knowledge'); // Adds the <<< string to the array items_in_backpack
+
+// Now the array is:
+// ['food', 'water', 'flashlight', 'GPS', 'javascript array push knowledge']
+
+var fastestTimes = [];
+
+// Assign the number 1 to the 10th element of the array nums.
+number[9] = 1;
+
+// Remove the last element from the array pets.
+pets.pop();
+
+// Create an empty array.
+
+let emptyArray = [];
+
+// Change the value of the first element of an array to a new string.
+
+emptyArray[0] = 'new string';
+
+// Add a new element, a number, to the end of an array.
+
+emptyArray.push(2);
+
+// Assign string values to the second and fourth elements of an array.
+
+emptyArray[1] = 'string 1';
+emptyArray[3] = 'string 2';
+
+// Add three number elements to end of an array.
+
+ newNumArray.push(1, 2, 3);
+
+//  Assign the number 1 to the 10th element of the array nums.
+newNumArray[9] = 1;
+
+// Time goal: 25 seconds. Create an empty array.
+
+newEmptyArray = [];
+
+// Change the value of the first element of an array to a new string.
+newEmptyArray[0] = 'new string that i should make up';
+
+// Add a new element, a number, to the end of an array.
+newEmptyArray.push(1);
+
+// Assign string values to the second and fourth elements of an array.
+
+newEmptyArray[1] = 'string 1';
+newEmptyArray[3] = 'string 2';
+
+// Add three number elements to the end of an array.
+newEmptyArray.push(1, 2, 3);
+
+// Live coding exercise:
+
+// Declare an empty array.
+// Assign a value to one of its elements.
+// Create an alert whose message is the element.
+// Click the Run It button to run your code live.
+
+let names = [];
+names[0] = 'justin';
+alert(names[0]);
+
+// Live coding exercise:
+
+// Code an array with 1 string element.
+// Add an additional element to the array using push.
+// Create an alert whose message is the last element.
+// Click the Run It button to run your code live.
+
+let arrayWithOneString = ['one string'];
+arrayWithOneString.push('second string');
+alert(arrayWithOneString[1]);
